d0ee1d692ed3333ab4a3468719a16002994b9e73
JOJO-CLOUD <596625032@qq.com>
2020-06-17 16:14:44 +0800

    V0.2

---
+++ app/src/main/java/cn/edu/scu/test20/LittleVideoActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.scu.test20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LittleVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_little_video);
+    }
+}
